e4f0e204d3b7974127b9f25833a8566dcd39a580
Z Abd Pro <abdihalim487@gmail.com>
2024-05-03 07:18:57 +0300

    Initial Author Route

--- server.js
+++ server.js
@@ -6,14 +6,19 @@ if(process.env.NODE_ENV !== 'production')
 const express = require("express")
 const indexRouter = require('./routes/index')
 
+const authorRouter = require('./routes/authors')
+
 const app = express()
 const expressLayouts = require("express-ejs-layouts")
 
+const bodyParser = require('body-parser')
 
 app.set('view engine','ejs')
 app.set('views', __dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
+
+app.use(bodyParser.urlencoded({limit:"10mb",extended: false}))
 app.use(express.static('public'))
 //mongoose
 const mongoose = require('mongoose')
@@ -25,18 +30,10 @@ mongoose.connect(process.env.DATABASE_URL,
 const db = mongoose.connection
 db.on('error',error =>console.error(error))
 db.once('open', () =>console.log('connected to mongoose'))
-// mongoose.connection.on("error", err => {
-
-//     console.log("err", err)
-  
-//   })
-//   mongoose.connection.on("connected", (err, res) => {
-  
-//     console.log("mongoose is connected")
-  
-//   })
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
+
 app.listen(process.env.PORT || 3000)
 
 
